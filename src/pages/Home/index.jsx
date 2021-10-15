325c860dd9100dbe29b8c503bc5e435dd18962d5
anirudha4 <anirudhag1999@gmail.com>
2021-10-15 16:28:25 +0530

    selectors-change

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Component from '@components/Home'
-import { useStore } from '@selectors/home'
+import { useStore } from './selectors'
+
 export default function Home() {
     const { results } = useStore();
     return (
